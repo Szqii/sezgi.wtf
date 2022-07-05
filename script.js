3e0cb3e72579436bd969fb48331a5702ad7b876b
Sezgi Altan <sezgialtan@Sezgis-Mac.local>
2022-07-05 17:10:53 +0300

    Add :: Functions to show/hide skill & contact list

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 const homeBtn = document.querySelector(".home-section-btn");
 const aboutBtn = document.querySelector(".about-section-btn");
 const projectsBtn = document.querySelector(".project-section-btn");
+const contactList = document.querySelector(".contact-list");
+const skills = document.querySelector(".skills");
+
+const checkContactList = () => {
+  if (contactList.classList.contains("hidden")) {
+    contactList.classList.remove("hidden");
+  }
+};
+
+const checkSkills = () => {
+  if (skills.classList.contains("hidden")) {
+    skills.classList.remove("hidden");
+  }
+};
 
 const observer = new IntersectionObserver(
   function (entries) {
@@ -14,19 +28,25 @@ const observer = new IntersectionObserver(
 
 const setButtonActive = (section) => {
   if (section === "home-section") {
+    skills.classList.add("hidden");
     homeBtn.classList.add("active");
     projectsBtn.classList.remove("active");
     aboutBtn.classList.remove("active");
+    checkContactList();
   }
   if (section === "about-section") {
+    skills.classList.add("hidden");
     homeBtn.classList.remove("active");
     projectsBtn.classList.remove("active");
     aboutBtn.classList.add("active");
+    checkContactList();
   }
   if (section === "project-section") {
+    contactList.classList.add("hidden");
     homeBtn.classList.remove("active");
     projectsBtn.classList.add("active");
     aboutBtn.classList.remove("active");
+    checkSkills();
   }
 };
 
