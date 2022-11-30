2d1b624f20fd786c36554105525f929dc20782fd
Szqii <szqii.altn@gmail.com>
2022-11-30 20:45:47 +0300

    Fix(projects.js) : Typo

--- src/data/projects.js
+++ src/data/projects.js
@@ -8,9 +8,9 @@ export const projectsArray = [
     githubURL: "https://github.com/Szqii/Bill-Splitter",
   },
   {
-    name: "Advive Generator",
+    name: "Advice Generator",
     description:
-      "A web application that allows users to split a bill between multiple people with customizable tip amount.",
+      "A web application that generates random advice for users, using Advice Slip API.",
     image: "advice-generator.png",
     websiteURL: "https://some-advice.netlify.app/",
     githubURL: "https://github.com/Szqii/Advice-Generator-App",
@@ -18,7 +18,7 @@ export const projectsArray = [
   {
     name: "Eilish Clothing",
     description:
-      "A web application that allows users to split a bill between multiple people with customizable tip amount.",
+      "A web application that allows create an account, login and manage their shopping cart.",
     image: "eilish-clothing.png",
     websiteURL: "https://eilish-clothing.netlify.app/",
     githubURL: "https://github.com/Szqii/Eilish-Clothing",
@@ -26,7 +26,7 @@ export const projectsArray = [
   {
     name: "Geo Geo",
     description:
-      "Geo is an app that you can find country infos with filtering by region and cool dark mode.",
+      "A web application that allows users to search for countries filtering by regions and input, with cool-looking dark mode. It also allows users to see informations about countries.",
     image: "geo-geo.png",
     websiteURL: "https://geo-geo.netlify.app/",
     githubURL: "https://github.com/Szqii/Geo",
@@ -34,7 +34,7 @@ export const projectsArray = [
   {
     name: "Pausiber Bank",
     description:
-      "A web application that allows users to split a bill between multiple people with customizable tip amount.",
+      "A web application that allows users to manage their bank account. It allows users to deposit and withdraw money",
     image: "pausiber-bank.png",
     websiteURL: "https://pausiber-bank.netlify.app",
     githubURL:
@@ -43,7 +43,7 @@ export const projectsArray = [
   {
     name: "Rock Paper Scissors Game",
     description:
-      "A web application that allows users to split a bill between multiple people with customizable tip amount.",
+      " A web application that allows users to play a game of Rock, Paper, Scissors against a computer.",
     image: "rps.png",
     websiteURL: "https://beat-me-rps.netlify.app/",
     githubURL: "https://github.com/Szqii/Rock-Paper-Scissors-Game",
@@ -51,7 +51,7 @@ export const projectsArray = [
   {
     name: "Easiest Todo App",
     description:
-      "A web application that allows users to split a bill between multiple people with customizable tip amount.",
+      "A web application that allows users to create a todo list. User can filter todos by all, active and completed.",
     image: "todos.png",
     websiteURL: "https://to-be-done.netlify.app",
     githubURL: "https://github.com/Szqii/react-todo-app",
@@ -59,7 +59,7 @@ export const projectsArray = [
   {
     name: "Worldwide Weather App",
     description:
-      "A web application that allows users to split a bill between multiple people with customizable tip amount.",
+      "A web application that allows users to search for weather conditions in a specific city around the world.",
     image: "wwwa.png",
     websiteURL: "https://wwwa.netlify.app",
     githubURL: "https://github.com/Szqii/Worldwide-Weather-App",
